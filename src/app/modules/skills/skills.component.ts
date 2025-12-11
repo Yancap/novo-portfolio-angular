7bdba0f9e492c1a80214489517fd566eba64becb
Yancap <yangabriel3001f@gmail.com>
2025-12-11 17:39:16 -0300

    add skill

--- src/app/modules/skills/skills.component.ts
+++ src/app/modules/skills/skills.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { SvgComponent } from "../../shared/svg/svg.component";
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [SvgComponent],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  public skills: { icon: string; tech: string }[] = [];
 }
